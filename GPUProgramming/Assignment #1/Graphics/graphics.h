deb3d009d3ee5bb0ce67bb5bea27745fc08cca4d
imnhk <koloke1712@gmail.com>
2020-05-12 09:52:11 +0900

    Add addignment 1

---
+++ GPUProgramming/Assignment #1/Graphics/graphics.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#define GLEW_STATIC
+//#define GLFW_INCLUDE_NONE
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+//#include <glm/gtx/transform.hpp>
+//#include <glm/gtc/type_ptr.hpp>
+//#include <glm/gtc/matrix_transform.hpp>
+
+#include <iostream>
+
+//#pragma comment (lib, "glew32s")
+//#pragma comment (lib, "opengl32")
+//#pragma comment (lib, "glfw3")
---
+++ GPUProgramming/Assignment #1/Graphics/obj.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <glm/glm.hpp>
+#include <vector>
+
+extern bool loadOBJ(
+	const char *path, 
+	std::vector<glm::vec3> &out_vertices, 
+	std::vector<glm::vec2> &out_uvs, 
+	std::vector<glm::vec3> &out_normals
+);
