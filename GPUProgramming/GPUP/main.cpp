ba9416eb357e6e21c59d725dd3932ab80cc49cf1
imnhk <koloke1712@gmail.com>
2020-05-16 13:08:02 +0900

    Draw quad on pass2

--- GPUProgramming/GPUP/main.cpp
+++ GPUProgramming/GPUP/main.cpp
@@ -1,11 +1,11 @@
 #include "main.h"
 
-Camera camera(glm::vec3(0.0f, 0.0f, 4.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
 double lastX = 0;
 double lastY = 0;
 
 enum RenderMode{NdotV, Silhouettes, BlurredSilouettes};
-RenderMode renderMode = NdotV;
+RenderMode renderMode = BlurredSilouettes;
 
 int main(int argc, char* argv[])
 {
@@ -42,27 +42,84 @@ int main(int argc, char* argv[])
 	// Shader를 불러와서 compile
 	Shader shader("src/vertex.shader", "src/fragment.shader");
 
-	// console 로 파일명을 입력받은 경우
+	// 모델 불러오기
+	// console argument 로 파일명을 입력받을 수 있다
 	string modelName;
 	if (argc > 1)
 		modelName = argv[0];
-	else 
+	else
 		modelName = "spot_triangulated.obj";
-
-	cout << "Loading model " << modelName << endl;
+	cout << "Loading model " << modelName << "..." << endl;
 	Model ourModel(modelName);
+	cout << "Loaded model " << modelName << endl;
 
-	// 렌더
+	
+
+
+	// fullscreen quad buffer
+	static const GLfloat quadVertexData[] = {
+		// position			// texCoord
+		-1.0f, -1.0f, 0.0f, 0.0f, 0.0f,
+		 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+		-1.0f,  1.0f, 0.0f, 0.0f, 1.0f,
+		 1.0f, -1.0f, 0.0f, 1.0f, 0.0f,
+		 1.0f,  1.0f, 0.0f, 1.0f, 1.0f
+	};
+
+	// renderTexture quad를 위한 VAO
+	unsigned int quadVAO;
+	glGenVertexArrays(1, &quadVAO);
+	glBindVertexArray(quadVAO);
+
+	unsigned int quadVertexBuffer;
+	glGenBuffers(1, &quadVertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, quadVertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(quadVertexData), quadVertexData, GL_STATIC_DRAW);
+	
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3*sizeof(float)));
+	glEnableVertexAttribArray(2);
+
+
+
+	// multi-pass render를 위한 gBuffer
+	unsigned int gBuffer = 0;
+	glGenFramebuffers(1, &gBuffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
+
+	unsigned int renderedTex;
+	glGenTextures(1, &renderedTex);
+	glBindTexture(GL_TEXTURE_2D, renderedTex);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, SCR_WIDTH, SCR_HEIGHT, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+
+	glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, renderedTex, 0);
+	unsigned int drawBuffers[1] = { GL_COLOR_ATTACHMENT0 };
+	glDrawBuffers(1, drawBuffers);
+
+	if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE)
+		return false;
+
+	// unbind framebuffer
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+
+
+
+	// Render loop
 	while (!glfwWindowShouldClose(window))
 	{
 		// 입력
 		processInput(window);
 
-		// render
-		glClearColor(0.99f, 0.99f, 0.99f, 1.0f);
+		glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		shader.use();
+		glUseProgram(shader.ID);
 
 		// projection mat 적용
 		glm::mat4 projection = glm::perspective(glm::radians(camera.Zoom), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
@@ -78,17 +135,47 @@ int main(int argc, char* argv[])
 		shader.setVec3("cameraPos", camera.Position);
 
 		if (renderMode == NdotV)
+		{
 			shader.setInt("pass", 0);
-		else if (renderMode == Silhouettes)
+			glBindVertexArray(ourModel.meshes[0].VAO);
+			glDrawElements(GL_TRIANGLES, ourModel.meshes[0].indices.size(), GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+
+		}
+		else if (renderMode == Silhouettes) {
 			shader.setInt("pass", 1);
+			//ourModel.Draw(shader);
+			glBindVertexArray(ourModel.meshes[0].VAO);
+			glDrawElements(GL_TRIANGLES, ourModel.meshes[0].indices.size(), GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+
+		}
 		else if (renderMode == BlurredSilouettes)
 		{
-			shader.setInt("pass", 2);
+			// renderedTex에 render
+			glBindFramebuffer(GL_FRAMEBUFFER, gBuffer);
+			glUniform1i(glGetUniformLocation(shader.ID, "pass"), 1);
+
+			glBindVertexArray(ourModel.meshes[0].VAO);
+			glDrawElements(GL_TRIANGLES, ourModel.meshes[0].indices.size(), GL_UNSIGNED_INT, 0);
+			glBindVertexArray(0);
+
+			// 다시 원래 framebuffer로 복구
+			glBindFramebuffer(GL_FRAMEBUFFER, 0);
+			glUniform1i(glGetUniformLocation(shader.ID, "pass"), 2);
+			
+			// quad 그리기
+			glBindTexture(GL_TEXTURE_2D, renderedTex);
+			glBindVertexArray(quadVAO);
+			glDrawArrays(GL_TRIANGLES, 0, 6);
+			glBindVertexArray(0);
+
+
+		
 		}
 		else
 			return -1;
 
-		ourModel.Draw(shader);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
@@ -141,7 +228,7 @@ void cursorCallback(GLFWwindow* win, double x, double y) {
 		camera.Position =
 			glm::rotate(camera.Yaw * 0.017f, glm::vec3(0, 1, 0)) *
 			glm::rotate(camera.Pitch * 0.017f, glm::vec3(1, 0, 0)) *
-			glm::vec4(0, 0, camDist, 0);
+			glm::vec4(0, 0, 3, 0);
 	}
 }
 
--- GPUProgramming/GPUP/main.h
+++ GPUProgramming/GPUP/main.h
@@ -26,4 +26,4 @@ void buttonCallback(GLFWwindow* win, int btn, int action, int mods);
 // »ó¼ö
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-const float camDist = 3;
+
